5d7733d7acccb6b791f53130cb8e8a6b409f8847
huashan <huashan@bj-m-203895a.local>
2016-08-11 11:49:08 +0800

    我的第一次提交2016-8-11

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by huashan on 16/8/11.
+//  Copyright © 2016年 LiHuashan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by huashan on 16/8/11.
+//  Copyright © 2016年 LiHuashan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
