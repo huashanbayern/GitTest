cc848b45f4e0cf0460886da3964e009d0e6c9c16
lihuashan001 <lihuashan001@lianjia.com>
2018-01-22 19:17:56 +0800

    测试

--- GitTest/AppDelegate.m
+++ GitTest/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    // 修改了注释
     return YES;
 }
 
